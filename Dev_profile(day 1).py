fca81a6d9aec8b5949440b2b720e9271d64195c3
Rancor06 <salmanamaricar@gmail.com>
2026-07-02 10:16:43 +0530

    Add files via upload

---
+++ Dev_profile(day 1).py
@@ -0,0 +1,19 @@
+#Step 1: Declare your variables
+name = "Salman Maricar"
+age = 19
+Learn = "Developer Profile Using Python"
+Experience = "Beginner"
+Goal = "Versatile Artificial Intelligence Professional"
+
+#Step 2: Check Data Types
+print ("Name Data Type: ",type(name))
+print ("Age Data Type :",type(age))
+
+#Step 3: Print the developer profile card
+print("========== DEVELOPER PROFILE CARD ==========")
+print(f"Name             : {name}")
+print(f"Age              : {age}")
+print(f"Learning Topic   : {Learn}")
+print(f"Coding Experience: {Experience}")
+print(f"Goal             : {Goal}")
+print("============================================")
