35860e3fd6bff3eca72c5ec039ef8b6745719bf4
Rancor06 <salmanamaricar@gmail.com>
2026-08-04 10:01:50 +0530

    Uploaded

---
+++ Day_30-Frontend/Frontend/quick_check/src/App.jsx
@@ -0,0 +1,12 @@
+import QuickRiskCheck from './QuickRiskCheck';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-shell">
+      <QuickRiskCheck />
+    </div>
+  );
+}
+
+export default App;
